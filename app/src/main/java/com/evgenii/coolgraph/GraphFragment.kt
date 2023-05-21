bd5dac270d662418a9f92a8e2fe68ff8f7e200df
ysshish6 <ysshish6@mts.ru>
2023-05-21 13:40:38 +0300

    configured project

---
+++ app/src/main/java/com/evgenii/coolgraph/GraphFragment.kt
@@ -0,0 +1,6 @@
+package com.evgenii.coolgraph
+
+import androidx.fragment.app.Fragment
+
+class GraphFragment: Fragment(R.layout.fragment_graph) {
+}
---
+++ app/src/main/java/com/evgenii/coolgraph/MainFragment.kt
@@ -0,0 +1,6 @@
+package com.evgenii.coolgraph
+
+import androidx.fragment.app.Fragment
+
+class MainFragment: Fragment(R.layout.fragment_main) {
+}
