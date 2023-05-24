0c261e501e4c4fb5433a04c66eef00efe096a906
ysshish6 <ysshish6@mts.ru>
2023-05-24 22:33:49 +0300

    refactoring

--- app/src/main/java/com/evgenii/coolgraph/common/CoroutineExt.kt
+++ app/src/main/java/com/evgenii/coolgraph/common/CoroutineExt.kt
@@ -1,13 +1,10 @@
 package com.evgenii.coolgraph.common
 
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.launch
 
 fun <T> createSingleEventFlow() = MutableSharedFlow<T>(
     replay = 0,
@@ -19,9 +16,7 @@ fun <T> Flow<T>.collect(
     lifecycleOwner: LifecycleOwner,
     block: (T) -> Unit
 ) {
-    lifecycleOwner.lifecycleScope.launch {
-        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
-            collect(block)
-        }
+    lifecycleOwner.lifecycleScope.launchWhenResumed {
+        collect(block)
     }
 }
